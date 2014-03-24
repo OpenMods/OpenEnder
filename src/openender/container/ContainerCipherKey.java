d55002d209c34fe474bd3bce9bd584a6fb72f3d0
Mikee Franklin <mikeefranklin@gmail.com>
2014-03-24 12:36:02 +0000

    Restricted the current slot on the cipher key

--- src/openender/container/ContainerCipherKey.java
+++ src/openender/container/ContainerCipherKey.java
@@ -3,6 +3,7 @@ package openender.container;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.IInventory;
+import net.minecraft.inventory.Slot;
 import openender.OpenEnder.Items;
 import openmods.container.ContainerBase;
 
@@ -14,7 +15,30 @@ public class ContainerCipherKey extends ContainerBase<Void> {
 		super(playerInventory, ownerInventory, null);
 		this.playerInventory = (InventoryPlayer)playerInventory;
 		addInventoryGrid(33, 31, 6);
-		addPlayerInventorySlots(85);
+		addPlayerInventorySlots(85);		
+	}
+	
+	@Override
+	protected void addPlayerInventorySlots(int offsetX, int offsetY) {
+		for (int row = 0; row < 3; row++)
+			for (int column = 0; column < 9; column++)
+				addSlotToContainer(new Slot(playerInventory,
+						column + row * 9 + 9,
+						offsetX + column * 18,
+						offsetY + row * 18));
+
+		for (int slot = 0; slot < 9; slot++) {
+			
+			final int currentSlot = slot;
+			
+			addSlotToContainer(new Slot(playerInventory, slot, offsetX + slot * 18, offsetY + 58) {
+				@Override
+			    public boolean canTakeStack(EntityPlayer par1EntityPlayer) {
+			        return currentSlot != playerInventory.currentItem;
+			    }
+			});
+			
+		}
 	}
 
 	public void onButtonClicked(EntityPlayer player, int buttonId) {
